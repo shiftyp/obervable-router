0b02e71b0388c25aee102cdfc131e6fb28a3a63f
Ryan Lynch <lynch.r.j@gmail.com>
2016-07-14 11:53:47 -0400

    Initial commit :rocket:

---
+++ README.md
@@ -0,0 +1,3 @@
+# Observable router
+
+A URL router based on RxJS Observables. Documentation will come soon.
---
+++ test/index.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+
+import { createRouter } from '../dist';
+
+test.todo('router should have an asObservable method');
+
+test.todo('router.route should match paths from history')
+
+test.todo('rotuer.route onNext callback should flat map to a promise')
+
+test.todo('router.route stream option should be flat mapped to router obsrvable');
+
+test.todo('router.route subroute callback should get a router as an argument');
+
+test.todo('router.route subrouter should match paths joined from parant route');
